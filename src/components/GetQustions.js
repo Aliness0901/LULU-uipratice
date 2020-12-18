13ada2e1855b0bcc1f79e689e5482a6fed172aa1
Aliness0901 <74873367+Aliness0901@users.noreply.github.com>
2020-12-18 16:52:50 -0500

    update

--- src/components/GetQustions.js
+++ src/components/GetQustions.js
@@ -19,7 +19,6 @@ export default function GetQustion(success, fail) {
             questionsdata.data=data.questions
             success();
             console.log(questionsdata);
-            console.log(data.questions);
             // return data                         //同之前拿用户的数据一样，return在这里只能return给getquestion这个函数
             //而不能return出去，所以最好的解决办法还是在判断读取成功了之后，在函数本身里拿
         });
--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,16 +1,16 @@
 import React, { PureComponent } from 'react'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import AskJumpButton from '../components/AskJumpButton'
-import {user_token} from '../views/Login'
+// import {user_token} from '../views/Login'
 import LikeTriButton from '../components/LikeTriButton'
 import GetQustion from '../components/GetQustions'
 
 import './MainPage.css'
 
 
-export let questionsdata={
-    data:[]             //这个data是一定要的，全局变量是questionsdata中data的部分，而questionsdata本身不是全局变量
+export let questionsdata = {
+    data: []             //这个data是一定要的，全局变量是questionsdata中data的部分，而questionsdata本身不是全局变量
 }           //如果你不写data这个属性的话，js就会随便给你开一个变量，有了这个data才能去使用，也就是说questionsdata相当于一个文件夹名字而已
 
 
@@ -21,41 +21,46 @@ class MainPage extends PureComponent {
         super(props)
 
         this.state = {
-            questionsdataRev:false
+            questionsdataRev: false
         }
     }
 
-    SuccessGet=()=>{
-        console.log(questionsdata.data);
+    SuccessGet = () => {
         this.setState({
-            questionsdataRev:true                       //尝试利用state来判断接收到数据后刷新页面
+            questionsdataRev: true                       //尝试利用state来判断接收到数据后刷新页面
         })
     }
 
-    componentDidMount=()=>{
+    componentDidMount = () => {
         GetQustion(this.SuccessGet);
     }
 
     render() {
-        console.log(user_token);                    //这里依旧有一个问题，就是只要页面刷新了，user_token就又空了
-        console.log(questionsdata.data);
+        // console.log(user_token);                    //这里依旧有一个问题，就是只要页面刷新了，user_token就又空了
         //这个刷新不是只react本身的刷新，而是用户点击浏览器的刷新，会丢失这个全局变量的内容
         return (
             <div>
-                <AskJumpButton/>
+                <AskJumpButton />
                 <header className='mainHeader'>
                     BIG FISH
                     {/* 把图片用作navlink，装饰背景 */}
-                    <NavLink to='/profile' className='userpic'/>           
+                    <NavLink to='/profile' className='userpic' />
                 </header>
                 <div className='afterheader_body'>
+                    <div className='Qustion_container'>
                     {
                         questionsdata.data.map(function (e) {
-                            return <div key={e.id}>{e.title}</div>
+                            return (
+                                <div className='small_Qustion_container' key={e.id}>
+                                    <h4 className='Qustion_title'>{e.title}</h4>
+                                    <div className='Qustion_detail'>{e.content}</div>
+                                    <LikeTriButton />
+                                </div>
+                            )
                         })
                     }
+                    </div>
                 </div>
-                <LikeTriButton/>
             </div>
         )
     }
