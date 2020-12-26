716e88af933a0aa0f48ebf4a7cadce4180e91186
Aliness0901 <74873367+Aliness0901@users.noreply.github.com>
2020-12-26 05:05:17 -0500

    1

--- src/views/Answers.js
+++ src/views/Answers.js
@@ -30,6 +30,7 @@ class Answers extends Component {                       //这里有purecomponent
         this.setState({
             answerdataRev: true                     //确认已经异步完成了之后，页面再次刷新
         })
+        answers.userinfo={}         //这里如果不刷新的话，后面拿取的时候就会对不上if，一定要刷新
     }
 
     FailAnswer = () => {
@@ -77,21 +78,15 @@ class Answers extends Component {                       //这里有purecomponent
                     </div>
                     {/* 早知道就所有的头部都写一个组件了，这样就省得再这里一遍又一遍重复了 */}
                    
+                
                     <div className='answers_body'>
-                        {answers.answer.map((e) => {
-                        //这里有一个问题就是当我们试图拿取的时候，有可能还没有返回这个数据，所以一定要考虑异步，如果没有拿取的话，这里的东西就是undefined
-                        //由于我们每次在fetch里调用success函数的时候，都会再次写入setstate，所以在不是purecomponent的情况下，会帮我们每拿取一次就刷新一次
-                        //所以在拿完全部的时候，如果不考虑下面的那个bug的话，就可以在拿取完成后，满足条件之后就再次刷新页面进去if
-                        //所以再次证明了fetch中回调函数的重要性，可以帮助我们刷新页面
-                        //目前来说，我们的主要刷新页面的手段就是setstate
-                            console.log(Object.keys(answers.userinfo).length);
-                            console.log(Object.keys(answers.answer).length);
-                            //输出结果是0 0 1 ， 也就是说到第三次刷新的时候才接收到
+                    {                   //这里是立即执行函数
+                       (
+                           ()=>{
                             let userinfoLength=Object.keys(answers.userinfo).length;
                             let answersLength=Object.keys(answers.answer).length;
-                            
-                        //这里其实还是有bug的，就是如果一个人回答了两次，有两个答案都是一个人的，就会出现不匹配的问题，但是这个先放着
                             if (userinfoLength===answersLength&&userinfoLength!==0) {               //这里的e就是answer拿出来的东西，所以404的时候answer里面是空，所以e就是空
+                                answers.answer.map((e)=>{
                                 return (
                                     console.log('进入渲染if'),
                                     <div className='each_answer' key={e.id}>
@@ -107,8 +102,8 @@ class Answers extends Component {                       //这里有purecomponent
                                         <div className='answer_detail'>{this.props.location.content}</div>
                                         <LikeTriButton like={e.number_of_likes} />
                                     </div>
-                                )
-                            }else if (e===[]){
+                                )})
+                            }else if (userinfoLength.length===0){
                                     return(
                                         <div key='none'>啊哦，还没有人答题哦</div>
                                     )
@@ -116,7 +111,12 @@ class Answers extends Component {                       //这里有purecomponent
                             else {
                                 return null                     //这里如果网页崩溃了，就在这里写else的情况
                             }
-                        })}
+                           }
+                       )()
+                   }
+                   {/* ****************************************** */}
+
+                        {/* ******************************************* */}
                     </div>
                 </div>
             </div>
@@ -126,5 +126,43 @@ class Answers extends Component {                       //这里有purecomponent
 
 export default Answers
 
+// {answers.answer.map((e) => {
+//     //这里有一个问题就是当我们试图拿取的时候，有可能还没有返回这个数据，所以一定要考虑异步，如果没有拿取的话，这里的东西就是undefined
+//     //由于我们每次在fetch里调用success函数的时候，都会再次写入setstate，所以在不是purecomponent的情况下，会帮我们每拿取一次就刷新一次
+//     //所以在拿完全部的时候，如果不考虑下面的那个bug的话，就可以在拿取完成后，满足条件之后就再次刷新页面进去if
+//     //所以再次证明了fetch中回调函数的重要性，可以帮助我们刷新页面
+//     //目前来说，我们的主要刷新页面的手段就是setstate
+//         console.log(Object.keys(answers.userinfo).length);
+//         console.log(Object.keys(answers.answer).length);
+//         //输出结果是0 0 1 ， 也就是说到第三次刷新的时候才接收到
 
+        
+
+//     //这里其实还是有bug的，就是如果一个人回答了两次，有两个答案都是一个人的，就会出现不匹配的问题，但是这个先放着
+//         if (userinfoLength===answersLength&&userinfoLength!==0) {               //这里的e就是answer拿出来的东西，所以404的时候answer里面是空，所以e就是空
+//             return (
+//                 console.log('进入渲染if'),
+//                 <div className='each_answer' key={e.id}>
+//                     <div className='difuser_title'>
+//                         {/*这里的父盒子display是row，竖着*/}
+//                         <div className='user_pic_ans' style={{backgroundImage:`url(${answers.userinfo[e.user_id].avatar_url})`}}></div>
+//                         <div className='user_detail'>
+//                             {/*这里的盒子display是column，横着*/}
+//                             <div>{answers.userinfo[e.user_id].name}</div>
+//                             <div>{answers.userinfo[e.user_id].created_at}</div>
+//                         </div>
+//                     </div>
+//                     <div className='answer_detail'>{this.props.location.content}</div>
+//                     <LikeTriButton like={e.number_of_likes} />
+//                 </div>
+//             )
+//         }else if (e===[]){
+//                 return(
+//                     <div key='none'>啊哦，还没有人答题哦</div>
+//                 )
+//         }
+//         else {
+//             return null                     //这里如果网页崩溃了，就在这里写else的情况
+//         }
+//     })}
 
